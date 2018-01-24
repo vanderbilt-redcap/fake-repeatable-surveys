5ecced1c519dffdee159fb19ad52f4094c0ad0f9
bascome <eva.bascompte.moragas@vanderbilt.edu>
2018-01-24 14:32:05 -0600

    New delete dags project

---
+++ DeleteDAGsExternalModule.php
@@ -0,0 +1,14 @@
+<?php
+namespace Vanderbilt\DeleteDAGsExternalModule;
+
+class DeleteDAGsExternalModule extends \ExternalModules\AbstractExternalModule{
+
+    public function __construct(){
+        parent::__construct();
+    }
+
+	function hook_save_record($project_id, $record){
+
+	}
+
+}
